671bc7e4342e465ea3429367bde51f69315ae156
zpwz <1226764058@qq.com>
2014-09-02 17:02:49 +0800

    创建项目提交

---
+++ src/text/A1.java
@@ -0,0 +1,14 @@
+package text;
+
+public class A1 {
+
+	public static void main(String[] args) {
+		int a[]={23,43,657};
+		int b[]={344,56,35,32,67};
+		b=a;
+		b[0]=111;
+		System.out.println(b[3]);
+		//printf("%d",a[0]);
+	}
+
+}
